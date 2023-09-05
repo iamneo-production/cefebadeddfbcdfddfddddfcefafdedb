712800f47ccaebd81d4dd5fe8f1ac225e48cb0b1
Iamneo <techadmin@iamneo.ai>
2023-09-05 17:06:23 +0000

    Backup Commit

--- jspservlet-app-binarytravels/src/main/java/services/FareCalculator.java
+++ jspservlet-app-binarytravels/src/main/java/services/FareCalculator.java
@@ -8,36 +8,36 @@ import model.Hotel;
 import model.Train;
 
 public class FareCalculator extends Booking{
-//Enter code here...
-public double book(Flight flight) {
 
-    double totalFare = booking(flight.getNoOfPersons(),flight.getRates());
+    public double book(Flight flight) {
 
-    if(flight.getTriptype().equalsIgnoreCase("one-way")){
+        double totalFare = booking(flight.getNoOfPersons(),flight.getRates());
+
+        if(flight.getTriptype().equalsIgnoreCase("one-way")){
+            return totalFare;
+        }else{
+            int comp = (flight.getFrom()).compareTo(flight.getTo());
+            if(comp < 0)
+                return 2 * totalFare;
+            else
+                return 0;
+        }
+    }
+
+    public double book(Bus bus) {
+        return booking(bus.getNoOfPersons(),bus.getRates());
+    }
+
+    public double book(Hotel hotel) {
+        double totalFare = 0;
+        if(hotel.getFromdate().isBefore(hotel.getTodate())){
+            totalFare = booking(hotel.getNoOfPersons(), hotel.getRates());
+        }
         return totalFare;
-    }else{
-        int comp = (flight.getFrom()).compareTo(flight.getTo());
-        if(comp < 0)
-            return 2 * totalFare;
-        else
-            return 0;
     }
-}
 
-public double book(Bus bus) {
-    return booking(bus.getNoOfPersons(),bus.getRates());
-}
-
-public double book(Hotel hotel) {
-    double totalFare = 0;
-    if(hotel.getFromdate().isBefore(hotel.getTodate())){
-        totalFare = booking(hotel.getNoOfPersons(), hotel.getRates());
+    public double book(Train train) {
+        return booking(train.getNoOfPersons(),train.getRates());
     }
-    return totalFare;
-}
-
-public double book(Train train) {
-    return booking(train.getNoOfPersons(),train.getRates());
-}
 
 }
